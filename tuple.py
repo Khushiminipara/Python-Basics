1009159e3322d77934cf89b0550760c1cb89d4ca
Khushiminipara <90308017+Khushiminipara@users.noreply.github.com>
2022-01-15 21:11:47 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,54 @@
+#Tuple
+
+# a. Write a Python program to create a tuple with different data types.
+
+# tuple = ("Khushi", 3.2, 1,False)
+# print(tuple)
+
+
+
+# b. Write a Python program to create a tuple 
+# with numbers and print one item.
+
+# colors = ("red", "pink", "blue", "green")
+# item = colors[2]
+# print(item)
+
+
+
+# c. Write a Python program to add an item in a tuple.
+
+# colors = ("red", "pink", "blue", "green")
+# print(colors)
+
+# A = list(colors)
+# A.append("white")
+# print(A)
+
+
+
+# d. Write a Python program to convert a tuple to a string.
+
+# tuple = ('p','r','i','y','a')
+# str = ''.join(tuple)
+# print(str)
+
+
+
+# e. Write a Python program to find the length of a tuple.
+
+# tuple = ('p','r','i','y','a')
+# print(len(tuple))
+
+# tup = tuple("Khushi Minipara")
+# print(tup)
+# print(len(tup))
+
+
+
+
+# Khushi Minipara
+# 20CS036
+# Github Link = https://github.com/Khushiminipara/Python-Basics.git
+
+
